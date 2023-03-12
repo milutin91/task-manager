e8e545d937c95e80976bd724069aad4bbb72f287
Milutin <milutin91@hotmail.com>
2023-03-12 19:46:17 +0100

    connect templates

--- src/main/java/com/example/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/example/taskmanager/repository/TaskRepository.java
@@ -7,11 +7,12 @@ import org.springframework.data.jpa.repository.Query;
 import java.time.LocalDateTime;
 import java.util.List;
 public interface TaskRepository extends JpaRepository<Task, Integer> {
-@Query(value = "SELECT assignee from tasks\n" +
+@Query(value = "SELECT assignee, count(*) AS HowMany from tasks\n" +
         "WHERE YEAR(due_date) = YEAR(CURRENT_DATE - INTERVAL 1 MONTH)\n" +
         "AND MONTH(due_date) = MONTH(CURRENT_DATE - INTERVAL 1 MONTH)\n" +
+        "AND assignee IS NOT NULL\n" +
         "\tGROUP BY assignee\n" +
-        "\tORDER BY count(*) desc\n" +
+        "\tORDER BY HowMany desc\n" +
         "    limit 5;", nativeQuery = true)
     List<Integer> findTopFiveEmployees();
 
